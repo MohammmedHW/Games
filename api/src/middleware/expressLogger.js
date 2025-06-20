557e7b8e6b121bc70c7e801328c734aeda703374
Vedansh Shrivastava <113520644+vedanshshri7@users.noreply.github.com>
2025-06-20 12:10:04 +0530

    Delete api directory

--- api/src/middleware/expressLogger.js
+++
@@ -1,7 +0,0 @@
-import { logger } from "../utils/logger";
-
-export default async (req, res, next) => {
-  logger.verbose(`new request: ${req.url} from ${req.ip}`);
-  //logger.info(res);
-  next();
-};
